591a1e677d61f4ed20ac039c013f9089a8ff9769
Ashish Bhosle <asbhosle2003@gmail.com>
2024-05-05 19:04:24 +0530

    new commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
-    <div className="border-b-2 border-b-orange-500 py-6">
+    <div className="border-b-4 border-b-orange-500 py-6">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
@@ -15,8 +16,7 @@ const Header = () => {
           <MobileNav />
         </div>
         <div className="hidden md:block">
-          
-         
+          <MainNav />
         </div>
       </div>
     </div>
